16c2b51d37e51dc1c9ec594446c8cb67c50b4610
Mrepp <me@mattrepp.com>
2025-10-21 21:17:54 -0700

    Trigger

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/college-interview-explorer/',
+  base: process.env.NODE_ENV === 'production' ? '/college-interview-explorer/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
