c890cff44a8e5b97e6193d1b4846c939501dba14
Mrepp <me@mattrepp.com>
2025-10-27 22:51:07 -0700

    Changed repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/college-interview-explorer/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/survey_git_test/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
