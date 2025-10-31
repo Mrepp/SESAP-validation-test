b75df931d1eb39e1cbb2ff0517b3e6c2403bba84
Matthew Repp <51304888+Mrepp@users.noreply.github.com>
2025-10-31 16:15:50 -0700

    Change base path for production environment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/survey_git_test/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/SESAP-validation-test/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
